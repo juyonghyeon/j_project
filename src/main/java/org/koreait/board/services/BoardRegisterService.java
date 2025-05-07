bb8483126dbcae40ff1ae41389dd10c9cc13c5d5
rmftp <kyj2001kyj@naver.com>
2025-05-07 12:38:06 +0900

    register1

---
+++ src/main/java/org/koreait/board/services/BoardRegisterService.java
@@ -0,0 +1,33 @@
+package org.koreait.board.services;
+
+import org.koreait.board.BoardSession;
+import org.koreait.board.entities.Board;
+import org.koreait.global.validators.Validator;
+import org.koreait.member.controllers.LoginForm;
+
+/**
+ * 로그인 처리 서비스
+ *
+ */
+public class BoardRegisterService {
+    private final Validator<EnrollForm> validator;
+    private final BoardInfoService infoService;
+
+    // 의존성 주입
+    public BoardLoginService(Validator<EnrollForm> validator, BoardInfoService infoService) {
+        this.validator = validator;
+        this.infoService = infoService;
+    }
+
+    // 로그인 처리
+    public void process(EnrollForm form) {
+
+        // 유효성 검사
+        validator.check(form);
+
+        // 로그인 처리 S
+        Board board = infoService.get(form.getSql());
+        BoardSession.setBoard(board);
+        // 로그인 처리 E
+    }
+}
---
+++ src/main/java/org/koreait/board/services/BoardUpdateService.java
@@ -0,0 +1,41 @@
+package org.koreait.board.services;
+
+import org.koreait.board.entities.Board;
+import org.koreait.board.mapper.BoardMapper;
+import org.koreait.global.configs.DBConn;
+import org.koreait.global.validators.Validator;
+import org.koreait.member.controllers.JoinForm;
+import org.mindrot.jbcrypt.BCrypt;
+
+public class BoardUpdateService {
+    private Board mapper;
+    private final Validator<JoinForm> validator;
+
+    public BoardUpdateService(BoardMapper mapper, Validator<JoinForm> validator) {
+        this.mapper = mapper;
+        this.validator = validator;
+    }
+
+    public void process(EnrollForm form) {
+        // 회원정보 수정 유효성 검사
+        validator.check(form);
+        Board board = new Board();
+        board.setTitle(BoardSession.getBoard().getTitle());
+        board.setTitle(form.getTitle());
+
+        // 비밀번호 변경인 경우 BCrypt 해시 변환
+        String content = form.getContent();
+        if (content != null && !content.isBlank()) {
+            board.setContent(BCrypt.hashpw(content, BCrypt.gensalt(12)));
+        }
+
+        mapper.update(board); // 수정처리
+
+        // mybatis는 생성된 mapper 조회 결과를 캐싱하므로 이를 갱신 해야 함
+        mapper = DBConn.getInstance().getSession().getMapper(BoardMapper.class);
+
+        // 로그인한 회원 정보도 갱신한다.
+        board = mapper.get(board.getSql()).orElse(null);
+        BoardSession.setBoard(board);
+    }
+}
