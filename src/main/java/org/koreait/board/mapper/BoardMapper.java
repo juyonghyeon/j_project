573a9793ecd2302b8e95c0e3ce4126be27605ca6
rmftp <kyj2001kyj@naver.com>
2025-05-07 12:02:31 +0900

    mapper

---
+++ src/main/java/org/koreait/board/mapper/BoardMapper.java
@@ -0,0 +1,20 @@
+package org.koreait.board.mapper;
+
+import org.apache.ibatis.annotations.Select;
+import org.koreait.board.entities.Board;
+import org.koreait.global.paging.SearchForm;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BoardMapper {
+    int register(Board board);
+    List<Board> getList(SearchForm search);
+    Optional<Board> get(String sql);
+
+    @Select("SELECT COUNT(*) FROM member WHERE email=#{email}")
+    int exists( String sql);
+
+    // 회원정보 수정
+    int update(Board board);
+}
